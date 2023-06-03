f5ab1f73aaed59ca29dcd7d0ca3d51c089db6ea2
miranda.lin-guignard <miranda.w.lin@gmail.com>
2023-06-03 21:12:27 +0200

    test: withdrawal

--- src/bankBasic/Account.spec.ts
+++ src/bankBasic/Account.spec.ts
@@ -7,10 +7,18 @@ describe("When having an account", () => {
     const balance = account.getBalance()
     expect(balance).toBe(0);
   });
-  it('should be able to do a deposit and increase the current balance by the deposit', function () {
+  it('should be able to do a deposit and increase the current balance by the deposit', () => {
     const account = new Account();
     account.deposit(100);
     const balance = account.getBalance()
     expect(balance).toBe(100);
   });
+
+  it('should be able to withdraw and decrease the balance of the account', () => {
+    const account = new Account();
+    account.deposit(100);
+    account.withdraw(10);
+    const balance = account.getBalance()
+    expect(balance).toBe(90);
+  })
 });
